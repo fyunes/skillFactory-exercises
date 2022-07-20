9e681559411f98ff3b4de6063c23188198252fb0
Franco Yunes <franco.yunes@hotmail.com>
2022-07-20 20:44:27 -0300

    :tada:Added Intro To JS Ex

---
+++ Exercise-2-JS/script.js
@@ -0,0 +1,18 @@
+let myName = "Franco";
+let myAge = 32;
+let isSingle = false;
+let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let namesArray = ["Melina", "Matias", "Williams","Franco", "Debora"];
+let newObject = {
+    name: "Franco",
+    age: 32,
+    isSingle: false, 
+    numberArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    namesArray: ["Melina", "Matias", "Williams","Franco", "Debora"]
+ };
+console.log(myName);
+console.log(myAge);
+console.log(isSingle);
+console.log(numberArray);
+console.log(namesArray);
+console.log(newObject);
