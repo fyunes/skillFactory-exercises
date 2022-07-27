f010d3c822236023bce1f8730ede1693a27a7e00
Franco Yunes <franco.yunes@hotmail.com>
2022-07-27 09:41:50 -0300

    :sparkles:added more styles

--- Week3/scripts/script.js
+++ Week3/scripts/script.js
@@ -18,28 +18,28 @@ if (Number.isNaN(firstNum) || Number.isNaN(secondNum)) {
   console.log("Ingrese solo números");
 }
 else if (firstNum > secondNum) {
-  console.log("El número mas grande es: " + firstNum);
+  console.log(`%cEl número mas grande es: ${firstNum}`, 'color: #da55cd; font-size: 20px; font-weight: bold;');
 }
 else if (secondNum > firstNum) {
-  console.log("El número mas grande es: " + secondNum);
+  console.log(`%cEl número mas grande es: ${secondNum}`, 'color: #da55cd; font-size: 20px; font-weight: bold;');
 }
 else {
-  console.log("Los números son iguales.");
+  console.log(`%cLos números son iguales.`, 'color: #da55cd; font-size: 20px; font-weight: bold;');
 };
-
-
-
+console.log('');
+console.log('%cA continuación comienza la practica con algunos metodos de arrays', 'color: #5569da; font-size: 22px; ');
+console.log('');
 const skillers = ["Rolo", "Javi", "Mel", "Ezequiel", "Max"];
 
-const mayusSkillers = skillers.map(skillers => skillers.toUpperCase());
-console.log(`Skillers en Mayuscula: ${mayusSkillers}`);
+// const mayusSkillers = skillers.map(skillers => skillers.toUpperCase());
+// console.log(`Skillers en Mayuscula: ${mayusSkillers}`);
 
 console.log('%cEste es el metodo .length 📜', 'color: #f4d714; font-size: 15px; ');
 // const totalSkillers = skillers.length (Quité la variable)
-console.log("Cantidad de Skillers:" + skillers.length);
+console.log(`Cantidad de Skillers: ${skillers.length}`);
 
 console.log('%cEste es el metodo .slice 📜', 'color: #f4d714; font-size: 15px; ');
-console.log("Solo quedó " + skillers.slice(3, 4));
+console.log(`Solo quedó ${skillers.slice(3, 4)}`);
 
 console.log('%cEste es el metodo .reverse 📜', 'color: #f4d714; font-size: 15px' );
 const skillersBackwards = skillers.reverse();
@@ -57,7 +57,7 @@ console.log(`Quedo nominado: ${removeLastSkiller} y se va de nuestro array`); //
 console.log('%cEste es el metodo .shift 📜', 'color: #f4d714; font-size: 15px');
 const removeFirstSkiller = skillers.shift();
 console.log(`Con shift eliminamos a... ${removeFirstSkiller}`); // con shift eliminamos el primer elemento del array
-console.log(`asi quedo el array por ahora.. ${skillers}`);
+console.log(`Así quedó el array por ahora.. ${skillers}`);
 
 console.log('%cEste es el metodo .unshift 📜', 'color: #f4d714; font-size: 15px');
 const addTwoSkillers = skillers.unshift("Matias", "Leila");
@@ -67,31 +67,30 @@ console.log(`El total hasta ahora es de ${addTwoSkillers} `);
 console.log('%cEste es el metodo .join 📜', 'color: #f4d714; font-size: 15px');
 console.log(`Vamos a separar el array con asteriscos ${skillers.join('*')} `);
 
-console.log('%cEste es el metodo ..concat 📜', 'color: #f4d714; font-size: 15px');
+console.log('%cEste es el metodo .concat 📜', 'color: #f4d714; font-size: 15px');
 const darkSideSkillers = ["Agustin", "Sofia", "Adrian", "Guillermo", "Alan"];
 const lotOfskillers = skillers.concat(darkSideSkillers);
 console.log(`Se agrando nuestra plantilla a:  ${lotOfskillers} `); //juntamos dos arrays y obtenemos uno nuevo
 
 console.log('%cEste es el metodo .sort 📜', 'color: #f4d714; font-size: 15px');
-console.log(` Estos son los skillers antes de usar el metodo sort:%c ${skillers}`, 'color: #55b0da');
+console.log(`Estos son los skillers antes de usar el metodo sort:%c ${skillers}`, 'color: #55b0da');
 console.log(`Veamos como queda luego de utilizarlo:%c ${skillers.sort()}`,  'color: #6bda55');
 
 console.log('%cEste es el metodo .flat', 'color: #f4d714; font-size: 15px');
-const skillersToFlat = ["Rolo", ["Javi", ["Mel", "Ezequiel", ["Max"]]]]; // quita el array que le sigue menos profundo, por indice comenzando del 1.
-console.log(skillersToFlat);
+const skillersToFlat = ["Rolo", "Javi", ["Mel", "Ezequiel", ["Max"]]]; // quita el array que le sigue menos profundo, por indice comenzando del 1.
 console.log(skillersToFlat.flat(1));
 
-console.log('%cEste es el metodo .from 📜', 'color: #f4d714; font-size: 15px');
-console.log(Array.from([skillers], skillers => skillers + " Volvemos a empezar " + skillers));
+// console.log('%cEste es el metodo .from 📜', 'color: #f4d714; font-size: 15px'); 
+// console.log(Array.from([skillers], skillers => skillers + ". Volvemos a empezar, " + skillers)); //esto no tiene ningun sentido, solo es práctica
 
-console.log('%cEste es el metodo .some', 'color: #f4d714; font-size: 15px');
+console.log('%cEste es el metodo .some', 'color: #f4d714; font-size: 15px'); //Si algun componente del array es menor a 4 devuelve true, sino, false (esperado: true)
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const some = numeros.some(checkNum)
 function checkNum(menor) {
   return menor < 4
 }
 console.log(some);
-console.log('%cEste es el metodo .every 📜', 'color: #f4d714; font-size: 15px');
+console.log('%cEste es el metodo .every 📜', 'color: #f4d714; font-size: 15px'); //si todos los componentes del array son menores a 4 devuelve true, sino false (esperado: false)
 const every = numeros.every(checkNum)
 function checkNum(menor) {
   return menor < 4
@@ -102,8 +101,19 @@ console.log('%cEste es el metodo .isArray aplicado a skillers[] 📜' , 'color: 
 console.log(Array.isArray(skillers)); //true
 
 console.log('%cEste es el metodo .at 📜', 'color: #f4d714; font-size: 15px');
-console.log(`Hola ${skillers.at(-4)}`); // con at accedemos a elementos individuales de un array
+console.log(`Hola ${skillers.at(-4)}`); // con at accedemos a elementos individuales de un array, utilizando indices negativos accedemos de derecha a izquierda.
+console.log('%cEste es el metodo .splice 📜', 'color: #f4d714; font-size: 15px');
+console.log(`Con splice podemos agregar nuevos elementos (se agregan Tomas y Emi): ${skillers.splice(2,0, 'Tomas', 'Emi')}`);
+console.log(skillers)
+console.log(`Ahora los quitamos: ${skillers.splice(2,2)}`);
 
+console.log('%cEste es el metodo .valueOf 📜', 'color: #f4d714; font-size: 15px');
+console.log(`Utilizamos valueOf que nos devuelve el array sin modificaciones para observar como quedo: ${skillers.valueOf()}`);
 
+console.log('%cEste es el metodo .indexof 📜', 'color: #f4d714; font-size: 15px');
+console.log(`Averiguemos el indice de Rolo usando indexOf(): ${skillers.indexOf('Rolo')}`);
 
-console.log('%c JavaScript!!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,156) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+console.log('%cEste es el metodo .forEach 📜', 'color: #f4d714; font-size: 15px');
+console.log(`Con el forEach() podemos loggear cada elemento de un array así:`), skillers.forEach(element => console.log(element));
+
+console.log('%c **Avalith**', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,156) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
