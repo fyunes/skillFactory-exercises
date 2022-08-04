a177057579fa6ddbb91cdc8f2d7ece8b419c1ea3
Franco Yunes <franco.yunes@hotmail.com>
2022-08-04 20:05:03 -0300

    :art: improved hover over cards

--- Week4/fetch/scripts/script-fetch.js
+++ Week4/fetch/scripts/script-fetch.js
@@ -1,21 +1,20 @@
 const URL_CARDS = "https://jsonplaceholder.typicode.com/todos";
-fetch (URL_CARDS)
+fetch(URL_CARDS)
     .then((response) => response.json())
     .then((data) => showData(data));
-
 function showData(data) {
     let body = ``;
-    for (let i = 0; i < data.length; i++){
+    for (let i = 0; i < data.length; i++) {
         body += `<div class = "card">
             <h2 class="cardsTitle">${data[i].title}</h2>
-            <p>User ID: ${data[i].userId}</p>
-            <p>ID: ${data[i].id}</p>
-            <div class="showStatus">
-            <div class="overlayStatus">Status: ${data[i].completed}
+            <p><b>User ID:</b> ${data[i].userId}</p>
+            <p><b>ID:</b> ${data[i].id}</p>
+            <div class="showStatus">           
+            <div class=${data[i].completed === true ? "true" : "false"}>Status: ${data[i].completed}
             </div>
             </div>
             </div>            
         `;
-    }  
-document.getElementById("fetch-petition").innerHTML = body;
+    }
+    document.getElementById("fetch-petition").innerHTML = body;
 }
